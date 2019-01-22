75d490f56b96c86ad98a8aae0ba9340247915446
ismail aidar <slimaidarismail@gmail.com>
2019-01-22 19:24:16 +0100

    install rich text editor

--- src/urls.py
+++ src/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pages.urls')),
     path('', include('tours.urls')),
+    path('summernote/', include('django_summernote.urls')),
 ]
 
 if settings.DEBUG:
--- tours/admin.py
+++ tours/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from .models import Destination, Gallery, Tour, TourDetail
+from django_summernote.admin import SummernoteModelAdmin
+
+class TourDetailModelAdmin(SummernoteModelAdmin):  # instead of ModelAdmin
+    summernote_fields = '__all__'
 
 
 admin.site.register(Destination)
 admin.site.register(Gallery)
 admin.site.register(Tour)
-admin.site.register(TourDetail)
+admin.site.register(TourDetail, TourDetailModelAdmin)
