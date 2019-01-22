a9d7d1958bf040f9c9e8cc5b32b6637b11b3b0a3
ismail aidar <slimaidarismail@gmail.com>
2019-01-22 09:44:35 +0100

    tours list with filter by destination

--- tours/views.py
+++ tours/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Tour, TourDetail
 
-# Create your views here.
+class TourListView(ListView):
+    model = TourDetail
+    template_name = "tours/tour_list.html"
+    paginate_by = 12
+
+    def get_queryset(self):
+        destination = self.request.GET.get('destination') or None
+        if destination is not None:
+            return TourDetail.objects.filter(tour__destination__name=destination)
+        else:
+            return super().get_queryset()
+    
+    
+
