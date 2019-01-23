0501d2905c0675281d90688ba9930aa5f53756a9
ismail aidar <slimaidarismail@gmail.com>
2019-01-23 15:43:41 +0100

    Add privacy and policy

--- pages/urls.py
+++ pages/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.HomeView.as_view(), name="home_page"),
-    path('about', views.HomeView.as_view(), name="about_page"),
-    path('contact', views.HomeView.as_view(), name="contact_page"),
-    path('privacy', views.HomeView.as_view(), name="privacy_page"),
+    path('about', views.AboutView.as_view(), name="about_page"),
+    path('contact', views.ContactView.as_view(), name="contact_page"),
+    path('privacy', views.PrivacytView.as_view(), name="privacy_page"),
 ]
