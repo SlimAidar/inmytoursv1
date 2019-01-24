1f80f936d52f828c3aab0416b0efd6d0af823c98
ismail aidar <slimaidarismail@gmail.com>
2019-01-24 08:09:01 +0100

    update email contact us

--- pages/views.py
+++ pages/views.py
@@ -3,7 +3,7 @@ from django.views.generic import TemplateView
 from tours.models import Destination, Tour, TourDetail
 from .forms import ContactUsForm
 from django.core.mail import send_mail
-
+from django.conf import settings
 
 class HomeView(TemplateView):
     def get_context_data(self, **kwargs):
@@ -37,7 +37,7 @@ class ContactView(TemplateView):
                     form.data['subject'],
                 ),
                 form.data['email'],
-                ['slimaidarismail@gmail.com'],
+                [settings.EMAIL_HOST_USER],
                 fail_silently=False,
             )
             context = super().get_context_data(**kwargs)
